359d84d6d158f0f46344c97ed32181b1c448c0e0
Sushant <sushantbhagat561@gmail.com>
2025-02-23 22:33:15 +0530

    changes for make it responsive and styling

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -1,10 +1,8 @@
-"use client";
 export default function AdminPage() {
-    return (
-      <div>
-        <h1 className="text-3xl font-bold">Welcome to the Admin Panel</h1>
-        <p className="mt-4">Use the sidebar to manage questions and tests.</p>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <h1 className="mytxtdim text-3xl font-bold">Welcome to the Admin Panel</h1>
+      <p className="mt-4">Use the sidebar to manage questions and tests.</p>
+    </div>
+  );
+}
