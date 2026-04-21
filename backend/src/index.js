fe25078d597d8c96b697dcf6cce94c75de104182
Ronit Raj <ronitk964@gmail.com>
2026-04-21 07:58:52 +0530

    refactor: simplify dotenv configuration to default path

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,10 +4,7 @@ import mongoose from 'mongoose';
 import {DB_NAME} from "./constants.js";
 import connectDB from './db/db.js';
 import {app} from './app.js'
-dotenv.config({
-    path: './.env'
-    
-})
+dotenv.config();
 
 connectDB()
 .then((connection) => {
